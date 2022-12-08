30c8c96594bd288ae7c2102a2302e8c410298fcc
Hanumareddi K B <hanumareddi.kb@quadwave.com>
2022-12-08 16:23:58 +0530

    1

---
+++ StudentGrade.py
@@ -0,0 +1,18 @@
+student_name = input("Enter student name: ")
+pointspossible = float(input("Enter total marks: "))
+score = float(input("Enter student score: "))
+
+student_percentage = (score/pointspossible)*100
+
+if student_percentage >= 90:
+    Grade = "A"
+elif student_percentage >= 80:
+    Grade = "B"
+elif student_percentage >= 70:
+    Grade = "C"
+elif student_percentage >= 60:
+    Grade = "D"
+else:
+    Grade = "F"
+
+print(f"Student Name is {student_name} and Grade obtained is {Grade}")
